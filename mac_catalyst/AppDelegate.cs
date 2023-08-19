09296dec3cbeb02c8aadcefe53a1073691fd7ae1
Valentin <valentin.zy@yandex.ru>
2023-08-19 22:24:07 +0300

    add mac catalyst example

---
+++ mac_catalyst/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using Shared;
+namespace mac_catalyst;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+        var deviceCount = VulkanLoader.CreateVulkan(Array.Empty<string>());
+
+        // create a UIViewController with a single UILabel
+        var vc = new UIViewController ();
+		vc.View!.AddSubview (new UILabel (Window!.Frame) {
+			BackgroundColor = UIColor.SystemBackground,
+			TextAlignment = UITextAlignment.Center,
+			Text = $"Hello, Mac Catalyst! Vulkan device count = {deviceCount}",
+			AutoresizingMask = UIViewAutoresizing.All,
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		return true;
+	}
+}
+
