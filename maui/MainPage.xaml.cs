30d07eb4d767b456441e4b569cfcea3b5c1fdd37
Valentin <valentin.zy@yandex.ru>
2023-08-22 20:08:35 +0300

    Add MAUI example

---
+++ maui/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Shared;
+
+namespace maui;
+
+public partial class MainPage : ContentPage
+{
+	uint count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		//count = VulkanLoader.CreateVulkan(Array.Empty<string>());
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		CounterBtn.Text = $"Devices count: {count}";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
+
